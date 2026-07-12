b5fd1c091b26e787c957bb6cccc749e89724c2aa
Developer Dave <developerdave@Davids-MacBook-Air.local>
2026-07-12 14:08:08 -0600

    Import academic settings logger

--- lib/screens/academic_settings_screen.dart
+++ lib/screens/academic_settings_screen.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fb;
 
 import '../l10n/app_localizations.dart';
+import '../utils/app_logger.dart';
 
 class AcademicSettingsScreen extends StatefulWidget {
   const AcademicSettingsScreen({super.key});
